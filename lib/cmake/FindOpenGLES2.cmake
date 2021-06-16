a5f9c6c6b9a991d78cafeadfe6fed05e14be8942
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2021-06-16 15:09:36 +0200

    Add FindOpenGLES.

---
+++ lib/cmake/FindOpenGLES2.cmake
@@ -0,0 +1,27 @@
+# FindOpenGLES2.cmake
+#
+# Finds the OpenGLES2 library and its dependencies
+#
+# This will define the following variables
+#       OpenGLES2_FOUND
+#  and the following imported targets
+#       OpenGLES2::OpenGLES2
+#
+# Author: Jakub Duchniewicz - j.duchniewicz@gmail.com
+
+find_library(OPENGLES2_LIBRARY NAMES
+    GLESv2
+)
+
+find_package(PackageHandleStandardArgs REQUIRED)
+find_package_handle_standard_args(OpenGLES2
+    DEFAULT_MSG
+    OPENGLES2_LIBRARY)
+
+if (NOT TARGET OpenGLES2::OpenGLES2)
+    add_library(OpenGLES2::OpenGLES2 INTERFACE IMPORTED)
+    set_target_properties(IMPORTED_LOCATION ${OPENGLES2_LIBRARY}) # is it needed though?
+endif()
+
+mark_as_advanced(OPENGLES2_LIBRARY)
+
