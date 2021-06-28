356b23eba84ce684cda906243179cee21000193d
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2021-06-28 12:53:46 +0200

    Add EGL/GLES2 include directories.

--- lib/cmake/FindEGL.cmake
+++ lib/cmake/FindEGL.cmake
@@ -2,6 +2,9 @@
 #
 # Finds the EGL library and its dependencies
 #
+# If EGL_INCLUDE_DIR is not set, sets it to the default directory on the Imagination BBB image:
+#       /opt/Native_SDK/include/EGL
+#
 # This will define the following variables
 #       EGL_FOUND
 #  and the following imported targets
@@ -13,6 +16,10 @@ find_library(EGL_LIBRARY NAMES
     EGL
 )
 
+if (NOT DEFINED EGL_INCLUDE_DIR)
+    set(EGL_INCLUDE_DIR "/opt/Native_SDK/include/EGL")
+endif()
+
 find_package(PackageHandleStandardArgs REQUIRED)
 find_package_handle_standard_args(EGL
     DEFAULT_MSG
@@ -22,6 +29,8 @@ if (NOT TARGET EGL::EGL)
     add_library(EGL::EGL UNKNOWN IMPORTED)
     set_target_properties(EGL::EGL PROPERTIES
         IMPORTED_LOCATION "${EGL_LIBRARY}")
+    set_target_properties(EGL::EGL PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${EGL_INCLUDE_DIR}")
 endif()
 
 mark_as_advanced(EGL_LIBRARY)
--- lib/cmake/FindOpenGLES2.cmake
+++ lib/cmake/FindOpenGLES2.cmake
@@ -2,6 +2,9 @@
 #
 # Finds the OpenGLES2 library and its dependencies
 #
+# If GLES2_INCLUDE_DIR is not set, sets it to the default directory on the Imagination BBB image:
+#       /opt/Native_SDK/include/GLES2
+#
 # This will define the following variables
 #       OpenGLES2_FOUND
 #  and the following imported targets
@@ -13,6 +16,10 @@ find_library(OPENGLES2_LIBRARY NAMES
     GLESv2
 )
 
+if (NOT DEFINED GLES2_INCLUDE_DIR)
+    set(GLES2_INCLUDE_DIR "/opt/Native_SDK/include/GLES2")
+endif()
+
 find_package(PackageHandleStandardArgs REQUIRED)
 find_package_handle_standard_args(OpenGLES2
     DEFAULT_MSG
@@ -22,6 +29,8 @@ if (NOT TARGET OpenGLES2::OpenGLES2)
     add_library(OpenGLES2::OpenGLES2 UNKNOWN IMPORTED)
     set_target_properties(OpenGLES2::OpenGLES2 PROPERTIES
         IMPORTED_LOCATION "${OPENGLES2_LIBRARY}")
+    set_target_properties(OpenGLES2::OpenGLES2 PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${GLES2_INCLUDE_DIR}")
 endif()
 
 mark_as_advanced(OPENGLES2_LIBRARY)
