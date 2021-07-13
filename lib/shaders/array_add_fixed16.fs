59c1f7dec7c83df0bd3e3e9f558a60b46db4ccf4
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2021-07-13 11:12:46 +0200

    Working addition with fixed-point 16-bit.

--- lib/shaders/array_add_fixed16.fs
+++ lib/shaders/array_add_fixed16.fs
@@ -14,41 +14,30 @@ varying vec2 vTexCoord;
 
 float fixed_to_float(vec2 inp)
 {
-    float n = inp.x * 256.0 + inp.y;
+    float n = inp.y * 256.0 + inp.x;
     return float(n / float(fraction_divider));
 }
 
 vec2 float_to_fixed(float inp)
 {
-    vec2 ret;
+    vec2 ret = vec2(0);
     inp = inp * float(fraction_divider);
-    //ret.x = inp - inp / 256.0; // TODO fix it and port to RGB565
-    //
+    ret.x = inp - inp / 256.0;
     ret.y = inp / 256.0;
     return ret;
 }
 
 void main(void)
 {
-    // one texel is 2 16-bit values (a, b) (g, r)
+    // one texel is 2 16-bit fixed-point 11.5 values (g, r) (b, a)
     vec4 texel1 = texture2D(texture0, vTexCoord);
     vec4 texel2 = texture2D(texture1, vTexCoord);
-    //float a1 = fixed_to_float(vec2(texel1.a * 255.0, texel1.b * 255.0)); // maybe wrong order?
-    //float a2 = fixed_to_float(vec2(texel1.g * 255.0, texel1.r * 255.0));
-    //float b1 = fixed_to_float(vec2(texel2.a * 255.0, texel2.b * 255.0));
-    //float b2 = fixed_to_float(vec2(texel2.g * 255.0, texel2.r * 255.0));
-    float a1 = fixed_to_float(vec2(texel1.r, texel1.g)); // maybe wrong order?
+    float a1 = fixed_to_float(vec2(texel1.r, texel1.g));
     float a2 = fixed_to_float(vec2(texel1.b, texel1.a));
     float b1 = fixed_to_float(vec2(texel2.r, texel2.g));
     float b2 = fixed_to_float(vec2(texel2.b, texel2.a));
     a1 += b1;
     a2 += b2;
-    //b1 *= 2.0;
-    //vec2 r1 = float_to_fixed(a1);
-    //vec2 r2 = float_to_fixed(b1);
-    //gl_FragColor = vec4(texel1.r, texel1.g, texel1.b, texel1.a);
     gl_FragColor = vec4(float_to_fixed(a1), float_to_fixed(a2));
-    //gl_FragColor = vec4(float_to_fixed(a1), float_to_fixed(b1));
-    //gl_FragColor = vec4(r1, r2);
 }
 
