ea76f11393e16b81fad5d0a7d23a9c7d6b3c2ad0
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2021-07-14 13:41:50 +0200

    Add command line parsing to the benchmark.

--- lib/benchmarks/first/benchmark1.c
+++ lib/benchmarks/first/benchmark1.c
@@ -1,23 +1,53 @@
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>
 #include "gpgpu_gles.h"
 
-#define HEIGHT 4096
-#define WIDTH HEIGHT
+//#define HEIGHT 4096
+//#define WIDTH HEIGHT
 #define DEBUG 0
 
 // TODO: add time measurements
 
 void cpu_compute(float* a1, float* a2, float* res);
 void generate_data(float* in1, float* in2);
+void parse_args(int argc, char** argv);
+void print_data_f(float* a);
+void print_benchmark_names();
+int size_valid();
+int name_valid();
+
+typedef enum
+{
+    NONE = -1,
+    ARRAY_ADD_FLOAT,
+    ARRAY_ADD_FIXED16,
+} EBenchmarkType;
+
+typedef struct
+{
+    char* name;
+    EBenchmarkType type;
+} benchmark_t;
+
+benchmark_t benchmark_types[] = {
+    { .name = "array_add_float", .type = ARRAY_ADD_FLOAT }, { .name = "array_add_fixed16", .type = ARRAY_ADD_FIXED16 },
+};
+int benchmark_length = 2;
+
+int HEIGHT, WIDTH;
+char* NAME;
+EBenchmarkType TYPE;
 
 // first argument is the size of array/matrix used
 // second argument is the type of computation? TODO: add some args parsing?
-int main()
+int main(int argc, char** argv)
 {
     clock_t start;
     int gpu_time, cpu_time;
     srand((unsigned int)time(NULL));
+
+    parse_args(argc, argv);
 
     if (gpgpu_init(HEIGHT, WIDTH) != 0)
     {
@@ -35,13 +65,7 @@ int main()
 
 #if DEBUG
     printf("Data before computation: \n");
-    for (int i = 0; i < WIDTH * HEIGHT; ++i)
-    {
-        printf("%.1f ", a1[i]);
-        if ((i + 1) % WIDTH == 0)
-            printf("\n");
-    }
-    printf("\n");
+    print_data_f(a1);
 #endif
 
     start = clock();
@@ -52,13 +76,7 @@ int main()
 
 #if DEBUG
     printf("Contents after GPU addition: \n");
-    for (int i = 0; i < WIDTH * HEIGHT; ++i)
-    {
-        printf("%.1f ", res[i]);
-        if ((i + 1) % WIDTH == 0)
-            printf("\n");
-    }
-    printf("\n");
+    print_data_f(res);
 #endif
 
     start = clock();
@@ -67,13 +85,7 @@ int main()
 
 #if DEBUG
     printf("Contents after CPU addition: \n");
-    for (int i = 0; i < WIDTH * HEIGHT; ++i)
-    {
-        printf("%.1f ", res2[i]);
-        if ((i + 1) % WIDTH == 0)
-            printf("\n");
-    }
-    printf("\n");
+    print_data_f(res2);
 #endif
 
     printf("GPU time %f CPU time %f\n", ((float)gpu_time / (float) CLOCKS_PER_SEC), ((float)cpu_time/ (float) CLOCKS_PER_SEC));
@@ -100,4 +112,90 @@ void cpu_compute(float* a1, float* a2, float* res)
 {
     for (int i = 0; i < WIDTH * HEIGHT; ++i)
         res[i] = a1[i] + a2[i];
+}
+
+void parse_args(int argc, char** argv)
+{
+    int opt;
+
+    while ((opt = getopt(argc, argv, "s:n:")) != -1)
+    {
+        switch (opt)
+        {
+            case 's':
+                WIDTH = strtol(optarg, NULL, 10);
+                HEIGHT = WIDTH;
+                if (size_valid() != 0)
+                {
+                    fprintf(stderr, "-s (Size) must be > 0, power of 2 and not greater than 2048\n");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case 'n':
+                NAME = optarg;
+                if (name_valid() != 0)
+                {
+                    fprintf(stderr, "Invalid benchmark name\n");
+                    print_benchmark_names();
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-s] [size] [-n] [benchmark_name]\n", argv[0]);
+                print_benchmark_names();
+                exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void print_data_f(float* a)
+{
+    for (int i = 0; i < WIDTH * HEIGHT; ++i)
+    {
+        printf("%.1f ", a[i]);
+        if ((i + 1) % WIDTH == 0)
+            printf("\n");
+    }
+    printf("\n");
+}
+
+int size_valid()
+{
+    int ret = 0;
+    if ((WIDTH & (WIDTH - 1)))
+        ret = -1;
+    if (WIDTH == 0)
+        ret = -1;
+    if (WIDTH > 2048) // TODO: query size dynamically?
+        ret = -1;
+
+    return ret;
+}
+
+int name_valid()
+{
+    int ret = -1;
+
+    for (int i = 0; i < benchmark_length; ++i)
+    {
+        if (strcmp(NAME, benchmark_types[i].name) == 0)
+        {
+            ret = 0;
+            TYPE = benchmark_types[i].type;
+            break;
+        }
+    }
+    return ret;
+}
+
+void print_benchmark_names()
+{
+    fprintf(stderr, "Possible benchmark names: \n");
+    for (int i = 0; i < benchmark_length; ++i)
+    {
+        fprintf(stderr, "%s\t", benchmark_types[i].name);
+        if (i % 2 == 0)
+            fprintf(stderr, "\n");
+    }
+    fprintf(stderr, "\n");
 }
